54afccecf9d71cf96cddbf3c13639b7bbe9f5ab5
2c0f1 <69714528+2c0f1@users.noreply.github.com>
2021-07-05 02:17:36 -0300

    Update youtube-dl-terminal-playlist-downloader.py

--- youtube-dl-terminal-playlist-downloader.py
+++ youtube-dl-terminal-playlist-downloader.py
@@ -1,9 +1,9 @@
 import os
-
+n = ('\n')
 while True:
- print ('enter the youtube url:')
- url_youtube = input()
- path = os.chdir('/home/user/Downloads/Music')
+ url_youtube = input('entre com a url do youtube:'+n)
+ print ('baixando')
+ os.chdir('/home/user/Downloads/Music')
  x = os.popen('youtube-dl --yes-playlist --extract-audio --audio-format mp3 '+url_youtube)
- output = x.readlines()
- print ('converted to mp3')
+ print(x.read())
+ print ('convertido para mp3'+n)
