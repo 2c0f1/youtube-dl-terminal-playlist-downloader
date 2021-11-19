d0a80150fb1383a262d1752346f6b701f75d459a
2c0f1 <69714528+2c0f1@users.noreply.github.com>
2021-11-19 13:53:12 -0300

    Update youtube-dl-terminal-playlist-downloader.py

--- youtube-dl-terminal-playlist-downloader.py
+++ youtube-dl-terminal-playlist-downloader.py
@@ -1,12 +1,11 @@
 import os
-import time
 import subprocess
 n = ('\n')
 while True:
  url_youtube = input('enter the youtube url:'+n)
  print ('downloading')
  path = os.chdir('/home/user/Downloads/Music')
- x = subprocess.call('youtube-dl --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail '+str(url_youtube), shell=True)
- time.sleep(20)
- print ('converted to mp3'+n)
- time.sleep(1)
+ x = subprocess.call('youtube-dl --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail '+str(url_youtube),subprocess.PIPE,shell=True)
+ out =  x.communicate()
+ if '(None, None)' in str(out):
+  print ('converted to mp3'+n)
